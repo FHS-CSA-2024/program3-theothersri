3325dde6f0af607787ec747b93c3e9c1c8009fb2
Sricharan Pusarla <119269135+theothersri@users.noreply.github.com>
2024-08-13 09:34:03 -0700

    Update Program3.java
    inputs, lnegth widt perimeter area output

--- Program3.java
+++ Program3.java
@@ -4,7 +4,35 @@ import java.util.Scanner; //This imports the console scanner that reads user inp
 
 //Your code here:
 
+class Quadralateral {
+    public static void main(String[] args){
 
+        Scanner myObj = new Scanner(System.in);
+        
+        //take inputs
+        System.out.println("Enter the length: ");
+        Integer l = myObj.nextInt();
+        System.out.println("Enter the width: ");
+        Integer w = myObj.nextInt();
+        
+        //calculate area and perimeter
+        int area = 0;
+        int peri = 0;
+        area = l*w;
+        peri = 2*l+2*w;
+        
+        //output
+        System.out.println("The length is: "+l);
+        System.out.println("The width is: "+w);
+        System.out.println("The area is:" + area);
+        System.out.println("The perimeter is:" + peri);
+        
+        
+
+        
+        
+    }
+}
 
 
 //Paste console output below:
